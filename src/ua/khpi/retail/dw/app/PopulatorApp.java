562ad711e006fde10abc5b0744e3376d13921792
Andrii Kopp <kopp93@gmail.com>
2017-08-24 20:08:40 +0300

    Refactor runnable class

--- src/ua/khpi/retail/dw/app/PopulatorApp.java
+++ src/ua/khpi/retail/dw/app/PopulatorApp.java
@@ -2,19 +2,26 @@ package ua.khpi.retail.dw.app;
 
 import java.io.PrintWriter;
 
+import ua.khpi.retail.dw.CustomerPopulator;
+import ua.khpi.retail.dw.DatePopulator;
 import ua.khpi.retail.dw.ProductPopulator;
 import ua.khpi.retail.dw.SuppliedProductPopulator;
 import ua.khpi.retail.dw.SupplierPopulator;
-import ua.khpi.retail.dw.SupplyOrderPopulator;
+import ua.khpi.retail.dw.SoldProductPopulator;
 
 public class PopulatorApp {
 
 	public void runApp(String fileName) {
 		try (PrintWriter writer = new PrintWriter(fileName, "UTF-8")) {
 			new ProductPopulator().populate(writer);
+
 			new SupplierPopulator().populate(writer);
-			new SupplyOrderPopulator().populate(writer);
+			new CustomerPopulator().populate(writer);
+
+			new DatePopulator().populate(writer);
+
 			new SuppliedProductPopulator().populate(writer);
+			new SoldProductPopulator().populate(writer);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
