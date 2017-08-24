c8ff5f97d5e158de86b24c1eacc555b466f464a7
Andrii Kopp <kopp93@gmail.com>
2017-08-24 19:30:30 +0300

    Refactor date populator

--- src/ua/khpi/retail/dw/DatePopulator.java
+++ src/ua/khpi/retail/dw/DatePopulator.java
@@ -3,12 +3,13 @@ package ua.khpi.retail.dw;
 import java.io.PrintWriter;
 
 import ua.khpi.retail.dw.api.IPopulator;
+import ua.khpi.retail.dw.util.CalendarUtil;
 import ua.khpi.retail.dw.util.IdRandomizer;
 
 public class DatePopulator implements IPopulator {
 
 	public static final int DATE_SUPPLY_NUMBER = 200;
-	
+
 	public static final int DATE_SALE_NUMBER = 2000;
 
 	@Override
@@ -19,17 +20,32 @@ public class DatePopulator implements IPopulator {
 			int dayNumber = IdRandomizer.getRandomId(1, 28);
 			int monthNumber = IdRandomizer.getRandomId(1, 12);
 			int yearNumber = IdRandomizer.getRandomId(2011, 2016);
-			
-			int quarterNumber = 1;
-			
-			if (monthNumber >= 4) {
-				quarterNumber = 2;
-			}
+
+			int quarterNumber = CalendarUtil.getQuarterNumber(monthNumber);
+
+			String monthName = String.format("Month-%d", monthNumber);
 
 			writer.printf(
-					"INSERT INTO Customer (CustomerID, CustomerName, CustomerAddress, CustomerPhone) "
-							+ "VALUES (%d, '%s', '%s', '%s');\n",
-					customerID, customerName, customerAddress, customerPhone);
+					"INSERT INTO SupplyDate (DateID, DayNumber, MonthNumber, MonthNameUKR, MonthNameENG, MothNameRUS,"
+							+ "QuarterNumber, YearNumber) VALUES (%d, '%s', '%s', '%s');\n",
+					dateID, dayNumber, monthNumber, monthName, monthName, monthName, quarterNumber, yearNumber);
+		}
+
+		for (int i = 0; i < DATE_SALE_NUMBER; i++) {
+			int dateID = DATE_SUPPLY_NUMBER + i;
+
+			int dayNumber = IdRandomizer.getRandomId(1, 28);
+			int monthNumber = IdRandomizer.getRandomId(1, 12);
+			int yearNumber = IdRandomizer.getRandomId(2011, 2016);
+
+			int quarterNumber = CalendarUtil.getQuarterNumber(monthNumber);
+
+			String monthName = String.format("Month-%d", monthNumber);
+
+			writer.printf(
+					"INSERT INTO SupplyDate (DateID, DayNumber, MonthNumber, MonthNameUKR, MonthNameENG, MothNameRUS,"
+							+ "QuarterNumber, YearNumber) VALUES (%d, '%s', '%s', '%s');\n",
+					dateID, dayNumber, monthNumber, monthName, monthName, monthName, quarterNumber, yearNumber);
 		}
 	}
 }
